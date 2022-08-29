1a5de81607b957742aa8da59c501eebacaf4bdbe
Damarus <leblanc.jlj@gmail.com>
2022-08-28 21:39:51 -0400

    Updated mob spawn point structure by zone

--- modules/HorizonXI/sql/horizon_mob_spawn_sets.sql
+++ modules/HorizonXI/sql/horizon_mob_spawn_sets.sql
@@ -16,18 +16,3 @@ CREATE TABLE IF NOT EXISTS `mob_spawn_sets` (
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 ;
-
--- Modify the mob_spawn_points schema to include a spawnset column
-LOCK TABLE `mob_spawn_points` WRITE;
-
-ALTER TABLE `mob_spawn_points`
-    ADD COLUMN IF NOT EXISTS `zoneid`	INT(10) AS ((`mobid` - 4096 * 4096) / 4096) virtual AFTER `mobid`,
-	ADD COLUMN IF NOT EXISTS `spawnset` TINYINT(3) NULL DEFAULT '0' AFTER `zoneid`;
-
-	
--- TODO:  ADD INSERT COMMANDS ONCE READY
-
-
--- END
-
-UNLOCK TABLES;
