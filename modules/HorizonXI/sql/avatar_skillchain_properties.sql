12daa402f513c38ba0ece039d0c9e587195e269b
Damarus <leblanc.jlj@gmail.com>
2022-11-12 23:41:31 -0500

    Avatar BP up to level 50 to have SC properties

--- modules/HorizonXI/sql/avatar_skillchain_properties.sql
+++ modules/HorizonXI/sql/avatar_skillchain_properties.sql
@@ -1,10 +1,23 @@
 -- HorizonXI Era Accuracy
 -- Remove skillchain properties from avatar bloodpacts
+SET autocommit = 0;
+
 LOCK TABLES
-    `mob_skills` WRITE;
+    `mob_skills` AS s WRITE,
+	`abilities` AS a READ;
 
-UPDATE `mob_skills` 
-SET  `primary_sc` = 0, `secondary_sc` = 0, `tertiary_sc` = 0
-WHERE `mob_skill_flag` & 64 = 64 OR `mob_skill_flag` & 128 = 128;
+UPDATE
+	`mob_skills` s
+LEFT JOIN 
+	`abilities` a 
+	ON s.mob_skill_name = a.name
+SET
+	s.primary_sc = 0,
+	s.secondary_sc = 0,
+	s.tertiary_sc = 0
+WHERE 
+	(`mob_skill_flag` & 64 = 64 OR `mob_skill_flag` & 128 = 128)
+	AND a.level > 50;
 
+COMMIT;
 UNLOCK TABLES;
