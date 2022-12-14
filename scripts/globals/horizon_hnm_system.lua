143d7c0975a7fb08b9a5a17645d92b66376a4756
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-12-14 01:43:02 -0500

    Deletes ??? for pop HNMS

--- scripts/globals/horizon_hnm_system.lua
+++ scripts/globals/horizon_hnm_system.lua
@@ -1,10 +1,3 @@
------------------------------------
--- ID Requires for ??? removal
------------------------------------
-local dragonsAeryID   = require("scripts/zones/Dragons_Aery/IDs")
-local valleySorrowsID = require("scripts/zones/Valley_of_Sorrows/IDs")
-local behemothDomID   = require("scripts/zones/Behemoths_Dominion/IDs")
-
 hxi = hxi or { }
 hxi.hnm = hxi.hnm or { }
 
@@ -86,14 +79,6 @@ hxi.hnm.startup = function(zone)
     zone:setLocalVar("[HNM]Behemoth", behemoth)
     zone:setLocalVar("BehemothDay", bDay)
     zone:setLocalVar("BehemothZone", bZone)
-
-    if zone == xi.zone.BEHEMOTHS_DOMINION then
-        GetNPCByID(behemothDomID.npc.BEHEMOTH_QM):setStatus(xi.status.DISAPPEAR)
-    elseif zone == xi.zone.DRAGONS_AERY then
-        GetNPCByID(dragonsAeryID.npc.FAFNIR_QM):setStatus(xi.status.DISAPPEAR)
-    elseif zone == xi.zone.VALLEY_OF_SORROWS then
-        GetNPCByID(valleySorrowsID.npc.ADAMANTOISE_QM):setStatus(xi.status.DISAPPEAR)
-    end
 end
 
 hxi.hnm.onDeath = function(mob)
